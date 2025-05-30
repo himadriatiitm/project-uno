8c57fd0b790237d9985b82968b59e7c195787577
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-05-30 07:51:30 +0530

    feat:  conform to response model

--- main.py
+++ main.py
@@ -3,24 +3,33 @@ import llm
 import os
 from pathlib import Path
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI
+import base64
+
 
 class Request(BaseModel):
     question: str
     image: Optional[str]
 
 
-class Response(BaseModel):
+class Link(BaseModel):
     text: str
     url: str
 
+
+class Response(BaseModel):
+    answer: str
+    links: List[Link]
+
+
 def between_tags(string: str, rope: str) -> str:
-    rope_start = f'<{rope}>'
-    rope_end = f'</{rope}>'
+    rope_start = f"<{rope}>"
+    rope_end = f"</{rope}>"
     start = string.index(rope_start) + len(rope_start)
     end = string.index(rope_end)
     return string[start:end]
+
 
 db = sqlite_utils.Database("embeddings.db")
 model = llm.get_model("gpt-4o-mini")
@@ -29,31 +38,50 @@ collection = llm.Collection("project-uno", db, model_id=model)
 
 app = FastAPI()
 
+
 @app.post("/api/")
 async def post_root(request: Request) -> Response:
+    attachments = []
+    if request.image:
+        attachments.append(llm.Attachment(content=base64.b64decode(request.image)))
+
+    links = []
     notes = ""
-    similar = collection.similar(request.question, number=5)
+    similar = collection.similar(request.question, number=3)
     for id, entry in enumerate(similar):
         name, _fragment = entry.id.split("#")
         context = Path(name).read_text()
         notes += "\n" + context
+        response = model.prompt(
+            f"""
+{context}
+
+Answer only from the above notes.
+Cite verbatim from notes in <text></text> tags.
+Cite the url above the text block for your chosen answer in <url></url> tags.
+If the answer is not in the context, answer "<unknown>"
+
+{request.question}
+""",
+            attachments=attachments,
+        ).text()
+        if "<unknown>" in response:
+            continue
+
+        url = between_tags(response, "url")
+        text = between_tags(response, "text")
+        links.append(Link(text=text, url=url))
 
     response = model.prompt(
         f"""
 {notes}
 
 Answer only from the above notes.
-Cite verbatim from notes in <text></text> tags.
-Cite the url above the text block for your chosen answer in <url></url> tags.
-If the answer is not in the context, answer "I don't know."
+Write your final answer in <answer></answer> tags.
+If the answer is not in the context, respond with <answer>I don't know</answer>.
 
 {request.question}
-""",
-        # TODO:
-        # attachments=[
-        #     llm.Attachment(content=b'somebinaryblob')
-        # ]
+"""
     ).text()
-    url = between_tags(response, "url")
-    text = between_tags(response, "text")
-    return Response(text=text, url=url)
+    answer = between_tags(response, "answer")
+    return Response(answer=answer, links=links)
